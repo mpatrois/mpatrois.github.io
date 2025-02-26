4cc52b17f2aba3c63981191091f1992e9f99e348
mpatrois <emmanuel.patrois@gmail.com>
2025-02-26 13:36:39 +0100

    Update font

--- build_static.sh
+++ build_static.sh
@@ -1,6 +1,7 @@
 git clone -b add-audio-engine git@github.com:mpatrois/tha-mood-web.git deps/tha-mood-web
 
 cd deps/tha-mood-web
+git pull
 npm install
 npm run build -- --base=/projects/mood
 cd ../..
