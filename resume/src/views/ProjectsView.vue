83616575cf92e87837090ccfcb998f1858f3a38f
mpatrois <emmanuel.patrois@gmail.com>
2025-03-08 14:39:44 +0100

    Edit tha-mood

--- resume/src/views/ProjectsView.vue
+++ resume/src/views/ProjectsView.vue
@@ -18,7 +18,7 @@ import IconWebGPU from '@/components/icons/IconWeb_GPU.vue';
   <main class="project-view mt2">
     <ProjectItem>
       <template #icon>
-        <a>
+        <a href="/projects/mood/index.html">
           <img width="100%" src="@/assets/Tha-Mood-Preview.png">
         </a>
       </template>
@@ -34,6 +34,8 @@ import IconWebGPU from '@/components/icons/IconWeb_GPU.vue';
 
       A virtual analog synthesizer developed in C++ using the JUCE framework.<br>
       Available as a VST, AU, and Standalone application.<br>
+      However, please note that I cannot distribute it at the moment as I do not have the required licenses. Send me an
+      email, and I will send you the VST, AU, or Standalone version according to your OS. <br><br>
       The audio engine has also been ported to WebAssembly and integrated with a completely redesigned UI built with
       Vue.js.<br>
       <a class="flex justify-start items-center" href="/projects/mood/index.html">
@@ -43,12 +45,24 @@ import IconWebGPU from '@/components/icons/IconWeb_GPU.vue';
       <div>
         <h4 class="bold mt1 mb1">Tech stack :</h4>
         <ul class="tech-stack-list flex flex-wrap justify-left">
-          <li class="flex mr2 items-center"><IconCpp class="mr1"/>C++</li>
-          <li class="flex mr2 items-center"><IconJUCE class="mr1"/>JUCE</li>
-          <li class="flex mr2 items-center"><IconCMake class="mr1"/>CMake</li>
-          <li class="flex mr2 items-center"><IconWebAssembly class="mr1"/>WebAssembly</li>
-          <li class="flex mr2 items-center"><IconVue class="mr1"/>Vue.js</li>
-          <li class="flex mr2 items-center"><IconJS class="mr1"/>JavaScript</li>
+          <li class="flex mr2 items-center">
+            <IconCpp class="mr1" />C++
+          </li>
+          <li class="flex mr2 items-center">
+            <IconJUCE class="mr1" />JUCE
+          </li>
+          <li class="flex mr2 items-center">
+            <IconCMake class="mr1" />CMake
+          </li>
+          <li class="flex mr2 items-center">
+            <IconWebAssembly class="mr1" />WebAssembly
+          </li>
+          <li class="flex mr2 items-center">
+            <IconVue class="mr1" />Vue.js
+          </li>
+          <li class="flex mr2 items-center">
+            <IconJS class="mr1" />JavaScript
+          </li>
           <!-- <li class="flex mr1"><IconWebAssembly class="mr1"/> WebAssembly</li> -->
         </ul>
       </div>
@@ -78,10 +92,18 @@ import IconWebGPU from '@/components/icons/IconWeb_GPU.vue';
       <div>
         <h4 class="bold mt1 mb1">Tech stack :</h4>
         <ul class="tech-stack-list flex flex-wrap justify-left">
-          <li class="flex mr2 items-center"><IconCpp class="mr1"/>C++</li>
-          <li class="flex mr2 items-center"><IconWebAssembly class="mr1"/>WebAssembly</li>
-          <li class="flex mr2 items-center"><IconJS class="mr1"/>JavaScript</li>
-          <li class="flex mr2 items-center"><IconWebGPU class="mr1"/>WebGPU</li>
+          <li class="flex mr2 items-center">
+            <IconCpp class="mr1" />C++
+          </li>
+          <li class="flex mr2 items-center">
+            <IconWebAssembly class="mr1" />WebAssembly
+          </li>
+          <li class="flex mr2 items-center">
+            <IconJS class="mr1" />JavaScript
+          </li>
+          <li class="flex mr2 items-center">
+            <IconWebGPU class="mr1" />WebGPU
+          </li>
           <!-- <li class="flex mr1"><IconWebAssembly class="mr1"/> WebAssembly</li> -->
         </ul>
       </div>
@@ -107,9 +129,15 @@ import IconWebGPU from '@/components/icons/IconWeb_GPU.vue';
       <div>
         <h4 class="bold mt1 mb1">Tech stack :</h4>
         <ul class="tech-stack-list flex flex-wrap justify-left">
-          <li class="flex mr2 items-center"><IconJS class="mr1"/>JavaScript</li>
-          <li class="flex mr2 items-center"><IconPixijs class="mr1"/>PixiJS</li>
-          <li class="flex mr2 items-center"><IconReact class="mr1"/>React</li>
+          <li class="flex mr2 items-center">
+            <IconJS class="mr1" />JavaScript
+          </li>
+          <li class="flex mr2 items-center">
+            <IconPixijs class="mr1" />PixiJS
+          </li>
+          <li class="flex mr2 items-center">
+            <IconReact class="mr1" />React
+          </li>
         </ul>
       </div>
     </ProjectItem>
