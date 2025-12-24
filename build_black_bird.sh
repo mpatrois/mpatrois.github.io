e35d3daf373694fa1bbdc6a6fc49fb5a271738ec
mpatrois <emmanuel.patrois@gmail.com>
2025-12-24 15:14:26 +0100

    Add 3 more songs

---
+++ build_black_bird.sh
@@ -0,0 +1,9 @@
+git clone -b feature/song-demo  git@github.com:mpatrois/blackbird.git deps/blackbird
+cd ./deps/blackbird/
+git pull
+
+npm install
+npm run build -- --base=/projects/blackbird
+cd ../../
+rm -rf projects/blackbird
+cp -R deps/blackbird/dist projects/blackbird
